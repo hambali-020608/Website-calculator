8cd8d8084f2292f4088b7515ef7cd71cb97a12b4
hambali-020608 <subastianhambali@gmail.com>
2024-09-24 19:46:14 +0700

    styling calculator

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const display=document.getElementById("display")
 const handleclick=(params)=>{
     if (params === "="){
         display.value=eval(display.value)
+        if(display.value == undefined){
+            display.value =="error"
+        }
     }else if(params ==="C"){
         display.value=""
     }
     
     else{
         display.value += params
+        
     }
+
     
 }
